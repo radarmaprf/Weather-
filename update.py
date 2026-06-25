3b4a62c30c12dcd13568eca960d83f0cbaa3aeb7
radarmaprf <y697b8yzz2@privaterelay.appleid.com>
2026-06-25 14:40:30 +0500

    Update update.py

--- update.py
+++ update.py
@@ -1,5 +1,7 @@
 from app import update_cache
+import time
 
 if __name__ == '__main__':
+    start = time.time()
     update_cache()
-    print("✅ Кеш обновлён успешно.")
+    print(f"✅ Кеш обновлён за {time.time() - start:.2f} сек.")
